c7b2b60b81eb366ff3b04dd3af9336543c00ea32
James <james.al.whiteley@gmail.com>
2024-11-29 09:58:43 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='borc2',
+      version='2.0',
+      description='Bayesian Optimization with Reliability Constraints',
+      url='https://github.com/jamesalexwhiteley/borc2',
+      author='James Whiteley',
+      author_email='james.al.whiteley@gmail.com',
+      license='MIT',
+      packages=find_packages(), 
+      zip_safe=False) 
